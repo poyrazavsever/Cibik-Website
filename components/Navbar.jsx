25881b238cbc77f9be2dd99aa10696986ce145be
poyrazavsever <poyrazavsever@hotmail.com>
2025-03-25 02:02:43 +0300

    feat: created Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { FaYoutube, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-normal-beige text-neutral-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
 
-export default Navbar
+        {/* Logo */}
+        <div>
+            <img src="Logos/cibik.png" alt="logoForNavbar" className="w-16" />
+        </div>
+
+        {/* Links */}
+        <ul className="flex space-x-6">
+
+          <li>
+            <a href="#" className="hover:text-dark-green transition-all">Ana Sayfa</a>
+          </li>
+
+          <li>
+            <a href="#" className="hover:text-dark-green transition-all">Hakkımızda</a>
+          </li>
+
+          <li>
+            <a href="#" className="hover:text-dark-green transition-all">Projelerimiz</a>
+          </li>
+
+          <li>
+            <a href="#" className="hover:text-dark-green transition-all">İletişim</a>
+          </li>
+
+        </ul>
+
+        {/* Sosyal Medya */}
+        <div className="flex space-x-4">
+          <a href="#" className="text-xl hover:text-red-600 transition-all">
+            <FaYoutube />
+          </a>
+          <a href="#" className="text-xl hover:text-pink-600 transition-all">
+            <FaInstagram />
+          </a>
+          <a href="#" className="text-xl hover:text-blue-600 transition-all">
+            <FaLinkedin />
+          </a>
+        </div>
+
+      </div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
